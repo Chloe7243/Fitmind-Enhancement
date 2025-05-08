e331615feeb3b85ad51b238e131f50008fd4ba26
BamSalt-code <sbolt555@outlook.com>
2025-05-08 10:46:57 +0100

    docs update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Fitmind'
+copyright = '2025, Bolt'
+author = 'Bolt'
 
 release = '0.1'
 version = '0.1.0'
